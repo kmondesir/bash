f72c3dd6dfeef647ad344e3eeae5186caa8c88a6
Kino Mondesir <owner@Kinos-MBP-15.lan>
2019-10-26 14:04:15 -0400

    Added link to GeeksforGeek

--- rsync_osx_to_win10.sh
+++ rsync_osx_to_win10.sh
@@ -19,29 +19,30 @@ win10pictures="/users/${remote_user}/documents"
 if [[ -d "${mount}" ]]; then
 
 # The script should be run with the following syntax: copy_osx_to_win10.sh '10.19.0.1' 'remote' 'C$' 'admin'
-
+# https://www.geeksforgeeks.org/du-command-linux-examples/
 # https://stackoverflow.com/questions/51715099/how-to-get-only-folder-size-from-du/51715324
 # local_size=$(du -sb --max-depth=0 ~/ | cut -f1)
 # target_size=$(du -sb --max-depth=0 ${mount}\${local_user} | cut -f1)
 
-test=$(du -bs ~/ | awk '{print $1}')
-control=$(du -bs ~/mnt | awk '{print $1}')
-echo "test :" $(($test/$megabyte)) 
-echo "control :" $(($control/$megabyte))
+test=$(sudo du -s ~/ | awk '{print $1}')
+control=$(sudo du -s ~/mnt | awk '{print $1}')
+echo "test :" $(($test/$megabyte)) " KB"
+echo "control :" $(($control/$megabyte)) " KB"
 
 	if [[ $test -gt $control ]]; then
 		
 		# https://www.ostechnix.com/the-mktemp-command-tutorial-with-examples-for-beginners/
-		
+		# 
 		# https://www.digitalocean.com/community/tutorials/how-to-use-rsync-to-sync-local-and-remote-directories-on-a-vps
-		rsync --archive --progress --partial-dir=${temp} ${osxdocuments}/* ${mount}/${win10desktop}	
-		rsync --archive --progress --partial-dir=${temp} ${osxdesktop}/* ${mount}/${win10desktop}
-		rsync --archive --progress --partial-dir=${temp} ${osxdownloads}/* ${mount}/${win10documents}
-		rsync --archive --progress --partial-dir=${temp} ${osxpictures}/* ${mount}/${win10pictures}
+		# rsync --archive --progress --partial-dir=${temp} ${osxdocuments}/* ${mount}/${win10desktop}	
+		# rsync --archive --progress --partial-dir=${temp} ${osxdesktop}/* ${mount}/${win10desktop}
+		# rsync --archive --progress --partial-dir=${temp} ${osxdownloads}/* ${mount}/${win10documents}
+		# rsync --archive --progress --partial-dir=${temp} ${osxpictures}/* ${mount}/${win10pictures}
 		sleep 5s
+		# echo "The local size is $((($test - $control)/$megabyte)) megabytes less than target"
 		exit 0
 	else
-		echo "The local size is greater $((($control - $test)/$megabyte)) than the target"
+		echo "The local size is $((($control - $test)/$megabyte)) megabytes greater than the target"
 		exit 1
 	fi
 else
