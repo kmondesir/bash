15f80d2b5cd7883a87f1090f91af46361ea69d03
Kino Mondesir <31377409+kmondesir@users.noreply.github.com>
2019-10-28 10:20:37 -0400

    Created read only variable for home directory

--- rsync/rsync_osx_to_osx.sh
+++ rsync/rsync_osx_to_osx.sh
@@ -9,6 +9,7 @@ remote_user=$1
 
 declare -r mount=~/mnt
 declare -r temp=~/tmp
+declare -r home=~/
 declare -r megabyte=1000000
 declare -r kilobyte=1000
 
@@ -17,9 +18,9 @@ if [[ true ]]; then
 # https://stackoverflow.com/questions/51715099/how-to-get-only-folder-size-from-du/51715324
 
 # test variable represents the source folder size
-test=$(sudo du -s ~/ | awk '{print $1}')
+test=$(sudo du -s ${home} | awk '{print $1}')
 # control variable represents the target folder size
-control=$(du -s ~/mnt | awk '{print $1}')
+control=$(du -s ${mount} | awk '{print $1}')
 
 echo "test :" $(($test/$megabyte)) " KB"
 echo "control :" $(($control/$megabyte)) " KB"
@@ -29,12 +30,12 @@ echo "control :" $(($control/$megabyte)) " KB"
 		# https://www.ostechnix.com/the-mktemp-command-tutorial-with-examples-for-beginners/
 		
 		# https://www.digitalocean.com/community/tutorials/how-to-use-rsync-to-sync-local-and-remote-directories-on-a-vps
-		rsync --archive --progress --partial-dir="${temp}" ${~/Documents} "$mount"/"$remote_user"	
-		rsync --archive --progress --partial-dir="${temp}" ${~/Desktop} "$mount"/"$remote_user"
-		rsync --archive --progress --partial-dir="${temp}" ${~/Downloads} "$mount"/"$remote_user"
-		rsync --archive --progress --partial-dir="${temp}" ${~/Pictures} "$mount"/"$remote_user"
-		rsync --archive --progress --partial-dir="${temp}" ${~/Movies} "$mount"/"$remote_user"
-		rsync --archive --progress --partial-dir="${temp}" ${~/Music} "$mount"/"$remote_user"
+		rsync --archive --progress --partial-dir="${temp}" "${home}/Documents" "$mount"/"$remote_user"	
+		rsync --archive --progress --partial-dir="${temp}" "${home}/Desktop" "$mount"/"$remote_user"
+		rsync --archive --progress --partial-dir="${temp}" "${home}/Downloads" "$mount"/"$remote_user"
+		rsync --archive --progress --partial-dir="${temp}" "${home}/Pictures" "$mount"/"$remote_user"
+		rsync --archive --progress --partial-dir="${temp}" "${home}/Movies" "$mount"/"$remote_user"
+		rsync --archive --progress --partial-dir="${temp}" "${home}/Music" "$mount"/"$remote_user"
 
 		"The local size is $((($test - $control)/$megabyte)) megabytes less than target"
 		sleep 5s
