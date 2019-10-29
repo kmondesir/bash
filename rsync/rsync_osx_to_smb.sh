1e7eea5022cba721d2e30409439597837bd1192c
Kino Mondesir <owner@Kinos-MBP-15.lan>
2019-10-29 07:27:48 -0400

    update

--- rsync/rsync_osx_to_smb.sh
+++ rsync/rsync_osx_to_smb.sh
@@ -22,9 +22,9 @@ declare -r osxmovies="${home}/Movies"
 declare -r osxmusic="${home}/Music"
 
 # test variable represents the source folder size
-test=$(sudo du -s ${home} | awk '{print $1}')
+test=$(sudo du -s "${home}" | awk '{print $1}')
 # control variable represents the target folder size
-control=$(sudo du -s ${mount} | awk '{print $1}')
+control=$(sudo du -s "${mount}" | awk '{print $1}')
 
 echo "test :" $(($test/$megabyte)) " KB"
 echo "control :" $(($control/$megabyte)) " KB"
